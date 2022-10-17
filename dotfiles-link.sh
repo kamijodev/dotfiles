5e5333b7fd84d787cab5c7a444bf716e32e3b8e5
kamijodev <kamijodev@gmail.com>
2022-10-18 00:26:26 +0900

    feat: ideavimrc

--- dotfiles-link.sh
+++ dotfiles-link.sh
@@ -2,4 +2,5 @@ ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/.warp ~/.warp
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
 
