ee45286af808592966491c462192cca38d4eecdd
kamijodev <f.kamijodev@gmail.com>
2026-02-04 10:23:44 +0900

    fix(nvim): インデントレインボーを背景色表示に変更
    - 線（│）からスペース背景色に変更
    - 控えめな暗い背景色で見やすく

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -94,14 +94,14 @@ return {
   {
     "machakann/vim-highlightedyank"
   },
-  -- インデントレインボー
+  -- インデントレインボー（スペース背景色）
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
       indent = {
-        char = "│",
+        char = " ",
         highlight = {
           "RainbowRed",
           "RainbowYellow",
@@ -112,18 +112,30 @@ return {
           "RainbowCyan",
         },
       },
+      whitespace = {
+        highlight = {
+          "RainbowRed",
+          "RainbowYellow",
+          "RainbowBlue",
+          "RainbowOrange",
+          "RainbowGreen",
+          "RainbowViolet",
+          "RainbowCyan",
+        },
+        remove_blankline_trail = false,
+      },
       scope = { enabled = false },
     },
     config = function(_, opts)
       local hooks = require("ibl.hooks")
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowRed", { bg = "#2d1f1f" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { bg = "#2d2a1f" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { bg = "#1f2535" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { bg = "#2d251f" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { bg = "#1f2d1f" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { bg = "#291f2d" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { bg = "#1f2a2d" })
       end)
       require("ibl").setup(opts)
     end,
